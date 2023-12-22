9c215244397799f60feb434077554cf1c972b264
77003 <aquamarine.ara@gmail.com>
2023-12-22 20:12:43 +0600

    k

--- task_5/signature_activate.py
+++ task_5/signature_activate.py
@@ -3,10 +3,10 @@ from signature_detector import *
 if __name__ == "__main__":
     # Пример использования
     detector = SignatureDetector()
-    jpeg_result = detector.detect_from_jpeg('path/to/your/jpeg_image.jpg')
-    pdf_result = detector.detect_from_pdf('path/to/your/pdf_document.pdf')
-    tiff_result = detector.detect_from_tiff('path/to/your/tiff_image.tiff')
+    jpeg_result = detector.detect_from_jpeg(r'task_5/teachers_signature/img_1.png')
+    #pdf_result = detector.detect_from_pdf('path/to/your/pdf_document.pdf')
+    #tiff_result = detector.detect_from_tiff('path/to/your/tiff_image.tiff')
 
     print(f"JPEG: Signature Detected - {jpeg_result}")
-    print(f"PDF: Signature Detected - {pdf_result}")
-    print(f"TIFF: Signature Detected - {tiff_result}")
+    #print(f"PDF: Signature Detected - {pdf_result}")
+    #print(f"TIFF: Signature Detected - {tiff_result}")
