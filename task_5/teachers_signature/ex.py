679ffb3277af5f8ac6929dc585bceec36075cb4d
77003 <aquamarine.ara@gmail.com>
2023-12-22 20:19:03 +0600

    k

---
+++ task_5/teachers_signature/ex.py
@@ -0,0 +1,23 @@
+from PIL import Image
+
+def split_image(image_path, output_directory):
+    img = Image.open(image_path)
+    width, height = img.size
+    cropped_images = []
+
+    sub_width = 150
+    sub_height = 100
+
+    for j in range(0, height, sub_height):
+        for i in range(0, width, sub_width):
+            box = (i, j, i + sub_width, j + sub_height)
+            cropped_img = img.crop(box)
+            cropped_images.append(cropped_img)
+            cropped_img.save(f"{output_directory}/img_{len(cropped_images)+8}.png")
+
+    return cropped_images
+
+# Пример использования функции
+image_path = r'task_5/teachers_signature/img.png'
+output_directory = r'task_5/teachers_signature'
+split_images = split_image(image_path, output_directory)
