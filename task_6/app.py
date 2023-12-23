926d1981c3cc286ecc8a85e7d4c55e7a726cc54f
77003 <aquamarine.ara@gmail.com>
2023-12-24 00:33:56 +0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	README.md

--- task_6/app.py
+++ task_6/app.py
@@ -1,52 +1,50 @@
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, render_template, request, jsonify
 import os
 import uuid
 from task_5.runner import detect_signatures_in_image
 
-
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'uploads'  # Папка для сохранения загруженных изображений
+UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-
-@app.route('/')
-def index():
-    return 'Hello, welcome to the signature detection service!'
 
 
 def save_image(image):
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
 
-    unique_filename = str(uuid.uuid4())  # Генерация уникального идентификатора
+    unique_filename = str(uuid.uuid4())
     image_path = os.path.join(app.config['UPLOAD_FOLDER'], unique_filename + '.jpg')
-    image.save(image_path)  # Сохранение изображения на сервере
+    image.save(image_path)
+    return unique_filename
 
-    return unique_filename  # Возвращаем уникальный идентификатор файла
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
-# Загрузка изображения
-@app.route('/api/upload', methods=['POST'])
+@app.route('/upload', methods=['POST'])
 def upload_image():
-    if 'image' not in request.files:
-        return jsonify({'error': 'No image provided'}), 400
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file provided'}), 400
 
-    image = request.files['image']
-    unique_filename = str(uuid.uuid4())  # Генерация уникального имени файла
-    image_path = os.path.join('uploads', unique_filename + '.jpg')
-    image.save(image_path)  # Сохранение загруженного изображения
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
 
-    # Вызываем вашу функцию для обработки загруженного изображения
+    file_id = save_image(file)
+    image_path = os.path.join(app.config['UPLOAD_FOLDER'], f"{file_id}.jpg")
+
+    # Обработка изображения для поиска подписей
     output_folder = 'processed_images'
-    detect_signatures_in_image('uploads', output_folder)
+    detect_signatures_in_image(image_path, output_folder)
 
-    processed_image_path = os.path.join(output_folder, unique_filename + '.jpg')
+    processed_image_path = os.path.join(output_folder, f"{file_id}.jpg")
     if os.path.exists(processed_image_path):
-        return send_file(processed_image_path, mimetype='image/jpeg'), 200
+        return jsonify({'processed_image': processed_image_path}), 200
     else:
         return jsonify({'error': 'Signature detection failed'}), 500
-
 
 
 if __name__ == '__main__':
